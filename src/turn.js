b8497efd76d8e650abb05cc5581fc5154511b852
Samuel Coleman <smcoleman19@gmail.com>
2019-08-31 16:17:06 -0600

    Add basic class and method setup for turn
    Co-authored-by: Eduardo Rodriguez <jeduardorjx@gmail.com>

---
+++ src/turn.js
@@ -0,0 +1,31 @@
+class Turn {
+	construtor(player) {
+		this.player = player;
+		this.guess = [];
+		this.endTurn = false;
+	}
+
+	spinWheel() {
+
+	}
+
+	buyVowel() {
+
+	}
+
+	guessConsonent() {
+
+	}
+
+	solvePuzzle() {
+
+	}
+
+	endTurn() {
+
+	}
+}
+
+if (typeof module !== 'undefined') {
+	module.exports = Turn;
+}
