997c02ba1cef840bee48ae7395aa0c8938dda4c0
Samuel Coleman <smcoleman19@gmail.com>
2019-09-02 16:50:31 -0600

    Test and manipulate methods for turn class
    Co-authored-by: Eduardo Rodriguez <jeduardorjx@gmail.com>
    Co-authored-by: Pol Sieira <polasp17@gmail.com>

--- src/turn.js
+++ src/turn.js
@@ -1,6 +1,8 @@
 class Turn {
-	constructor(player) {
+	constructor(player, puzzle, wheel) {
 		this.player = player;
+		this.puzzle = puzzle;
+		this.wheel = wheel;
 	}
 
 	spinWheel(consonant) {
@@ -18,28 +20,34 @@ class Turn {
 	buyVowel(vowel) {
 		if (this.player.currentScore >= 100) {
 			this.player.currentScore -= 100;
-			if (puzzle.correctAnswer.toUpperCase().includes(vowel.toUpperCase())) {
+			if (this.puzzle.correct_answer.toUpperCase().includes(vowel.toUpperCase())) {
+				return true;
 				// display each correct vowel on DOM
 			} else {
 				this.endTurn();
+				return false;
 			}
 		}
 	}
 
 	guessConsonant(consonant) {
-		if (puzzle.correctAnswer.toUpperCase().includes(consonant.toUpperCase())) {
+		if (this.puzzle.correct_answer.toUpperCase().includes(consonant.toUpperCase())) {
+			return true;
 			// display each correct vowel on DOM
 		} else {
 			this.endTurn();
+			return false;
 		}
 	}
 
 	solvePuzzle(guess) {
-		if (guess.toUpperCase() === puzzle.correctAnswer.toUpperCase()) {
-			player.totalScore += currentScore;
+		if (guess.toUpperCase() === this.puzzle.correct_answer.toUpperCase()) {
+			this.player.totalScore += this.player.currentScore;
+			return true;
 			// end Round method
 		} else {
 			this.endTurn();
+			return false;
 		}
 	}
 
--- test/turn-test.js
+++ test/turn-test.js
@@ -1,33 +1,48 @@
 import chai from 'chai';
 import Turn from '../src/turn.js';
 import Player from '../src/player.js';
+import data from '../src/data.js';
+import Round from '../src/round.js';
+import Game from '../src/game.js';
 const expect = chai.expect;
 
 describe('Turn', function() {
 	let turn;
-	let player1;
-	let player2;
-	let player3;
+	let player;
+	let round;
+	let game;
 	beforeEach(() => {
-		player1 = new Player('Bob');
-		player2 = new Player('Tim'); 
-		player3 = new Player('Jan');  
-    turn = new Turn([player1, player2, player3]);
+		game = new Game(data);
+		round = new Round(data, game.puzzleBank)
+		player = new Player('Bob');  
+    turn = new Turn(player, data.puzzles.one_word_answers.puzzle_bank[0], data.wheel);
   });
 
   it('should return true', function() {
     expect(true).to.equal(true);
   });
 
-  it ('should be a function', function() {
+  it('should be a function', function() {
   	expect(Turn).to.be.a('function');
   });
 
-  // it('should have a player', function() {
-  // 	expect(turn.players[0].name).to.equal('Bob')
-  // });
+  it('should have a player', function() {
+  	expect(turn.player.name).to.equal('Bob')
+  });
 
-  // it('should set a players score to zero if bankrupt', function() {
-  // 	expect(turn.players[0].name).to.equal('Bob')
-  // });
+  it('should be able to buy vowel', function() {
+  	turn.player.currentScore += 200;
+  	expect(turn.buyVowel('a')).to.equal(true);
+  	expect(turn.buyVowel('e')).to.equal(false);
+  })
+
+  it('should be able to guess consonant', function() {
+  	expect(turn.guessConsonant('m')).to.equal(true);
+  	expect(turn.guessConsonant('z')).to.equal(false);
+  })
+
+  it('should be able to solve the puzzle', function() {
+  	expect(turn.solvePuzzle('armchair')).to.equal(true);
+  	expect(turn.solvePuzzle('legchair')).to.equal(false);
+  })
 });
