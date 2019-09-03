6aabe31e0d5a53c0b8cb036db4b8050ec231bfaa
posi7790 <posi7790@colorado.edu>
2019-09-03 12:46:02 -0600

    Prepare to pull username from dom

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 import './css/normalize.css';
@@ -11,3 +10,5 @@ import './css/normalize.css';
 import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+/* ------------------------------------- */
