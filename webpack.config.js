eba2fc813e6008ed68434fec243e87aa2d0392df
posi7790 <posi7790@colorado.edu>
2019-09-03 12:55:54 -0600

    Fix spacing

--- webpack.config.js
+++ webpack.config.js
@@ -11,24 +11,21 @@ module.exports = {
   mode: 'development',
   // CSS and file (image) loaders
   module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'images/',
-                  publicPath: 'images/'
-                }
-              }
-            ]
-      }
+    rules: [{
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    },
+    {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'images/',
+          publicPath: 'images/'
+        }
+      }]
+    }
     ],
   },
   // Below is needed for webpack-dev-server
@@ -38,6 +35,6 @@ module.exports = {
     })
   ],
   devServer: {
-         contentBase: './dist'
+    contentBase: './dist'
   }
 };
