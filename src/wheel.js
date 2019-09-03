667acfdbc1706277511ba0a0e335e31730a7a108
posi7790 <posi7790@colorado.edu>
2019-09-03 09:19:29 -0600

    Slim down Wheel class for later use

--- src/wheel.js
+++ src/wheel.js
@@ -1,26 +1,7 @@
 // class Wheel {
 // 	constructor(wheelData) {
 // 		this.wheelData = [...wheelData];
-// 		this.randomizeWheel();
-// 	}
-
-// 	randomizeWheel() {
-// 		let temporaryValue;
-// 		let randomIndex;
-// 		let currentIndex = this.wheelData.length;
-//  		// While there remain elements to shuffle...
-//  		while (0 !== currentIndex) {
-//    	// Pick a remaining element...
-//    		randomIndex = Math.floor(Math.random() * currentIndex);
-//    		currentIndex -= 1;
-//    		// And swap it with the current element.
-//    		temporaryValue = this.wheelData[currentIndex];
-//    		this.wheelData[currentIndex] = this.wheelData[randomIndex];
-//    		this.wheelData[randomIndex] = temporaryValue;
-//  		}
 // 	}
 // }
 
-// if (typeof module !== 'undefined') {
-// 	module.exports = Wheel;
-// }
+// export default Wheel;
