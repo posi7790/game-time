89b967c0b5f40a5e607dd8a438445eb02cc88e43
posi7790 <posi7790@colorado.edu>
2019-09-03 09:02:45 -0600

    Clean up Round class

--- src/round.js
+++ src/round.js
@@ -1,44 +1,45 @@
 class Round {
-	constructor(data, puzzleBank) {
-		this.wheelData = [...data.wheel];
-		this.puzzleBank = puzzleBank;
-		this.currentRound = 0;
-		this.currentPlayer = 0;
-		this.currentPuzzle = {};
-	}
+  constructor(data, puzzleBank) {
+    this.wheelData = [...data.wheel];
+    this.puzzleBank = puzzleBank;
+    this.currentRound = 0;
+    this.currentPlayer = 0;
+    this.currentPuzzle = {};
+  }
 
-	randomizeWheel() {
-		let temporaryValue;
-		let randomIndex;
-		let currentIndex = this.wheelData.length;
- 		// While there remain elements to shuffle...
- 		while (0 !== currentIndex) {
-   	// Pick a remaining element...
-   		randomIndex = Math.floor(Math.random() * currentIndex);
-   		currentIndex -= 1;
-   		// And swap it with the current element.
-   		temporaryValue = this.wheelData[currentIndex];
-   		this.wheelData[currentIndex] = this.wheelData[randomIndex];
-   		this.wheelData[randomIndex] = temporaryValue;
- 		}
-	}
+  randomizeWheel() {
+    let temporaryValue;
+    let randomIndex;
+    let currentIndex = this.wheelData.length;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      // And swap it with the current element.
+      temporaryValue = this.wheelData[currentIndex];
+      this.wheelData[currentIndex] = this.wheelData[randomIndex];
+      this.wheelData[randomIndex] = temporaryValue;
+    }
+  }
 
-	endRound() {
-		this.currentRound++;
-		if (this.currentRound === 3) {
-				// end game or start bonus round
-		}
-	}
+  endRound() {
+    this.currentRound++;
+    if (this.currentRound === 3) {
+      // end game or start bonus round
+    }
+  }
 
-	choosePuzzle() {
-		this.currentPuzzle = this.puzzleBank.splice(this.getRandomInteger(this.puzzleBank.length - 1), 1)
-	}
+  choosePuzzle() {
+    let randomPuzzleIndex = this.getRandomInteger(this.puzzleBank.length - 1);
+    this.currentPuzzle = this.puzzleBank.splice(randomPuzzleIndex, 1);
+  }
 
-	getRandomInteger(max) {
-		return Math.floor(Math.random() * Math.floor(max))
-	}
+  getRandomInteger(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 }
 
 if (typeof module !== 'undefined') {
-	module.exports = Round;
+  module.exports = Round;
 }
